898d7a8b489825621d5b8776a3da49a124f8f77d
Andrew Collier <andrew@fathomdata.dev>
2022-07-03 05:34:50 +0100

    feat: Add category list

--- database/database/category.py
+++ database/database/category.py
@@ -1,8 +1,27 @@
 from .base import *
 
 
+CATEGORY_LIST = [
+    "K2 senior",
+    "K2 Junior",
+    "K2 Ladies",
+    "K2 Veteran",
+    "K2 Mixed",
+    "K1 Senior",
+    "K1 Junior",
+    "K1 Ladies",
+    "K1 Veteran",
+    "Junior/Veteran",
+    "C2",
+    "C1",
+]
+
+
 class Category(Base):
     __tablename__ = "category"
 
     id = Column(Integer, primary_key=True)
-    name = Column(String)
+    label = Column(String)
+
+    def __repr__(self):
+        return "Category('%s')" % (self.label)
