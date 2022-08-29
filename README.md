f2727f2e7fdf56dfaafae89ada70364b3ed5ca4d
Andrew Collier <andrew@fathomdata.dev>
2022-08-29 08:06:28 +0100

    feat: Add first views from templates

--- README.md
+++ README.md
@@ -7,3 +7,9 @@ Running the app:
 ```
 flask --app kanoe run
 ```
+
+During development you will want to run the app in debug mode:
+
+```
+flask --app kanoe --debug run
+```
--- kanoe/views.py
+++ kanoe/views.py
@@ -1,6 +1,22 @@
+from flask import render_template
+
 from . import app
+import database as db
+
+session = db.Session()
 
 
 @app.route("/")
 def index():
+    return render_template("index.html")
+
+
+@app.route("/boats")
+def boats():
     return "Hello World!"
+
+
+@app.route("/clubs")
+def clubs():
+    clubs = session.query(db.Club).all()
+    return render_template("clubs.html", clubs=clubs)
