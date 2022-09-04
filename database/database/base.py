382f4031953aafe45a275d0e4dcd0bae49c5f2e2
Andrew Collier <andrew@fathomdata.dev>
2022-09-04 06:43:48 +0100

    fix: Handle SQLite threads error
    The error was "SQLite objects created in a thread can only be used in that same thread".

--- database/database/base.py
+++ database/database/base.py
@@ -30,11 +30,14 @@ from sqlalchemy.sql import func, expression
 from sqlalchemy.schema import UniqueConstraint
 from sqlalchemy.exc import SQLAlchemyError, DataError, IntegrityError
 from sqlalchemy.orm.exc import NoResultFound
+from sqlalchemy.pool import SingletonThreadPool
 
 from .config import *
 
 CONNECTION_STRING = "sqlite:///kanoe.db"
-CONNECTION_ARGS = {}
+CONNECTION_ARGS = {
+    # "check_same_thread": False
+}
 
 LIMIT_DAILY = sqlalchemy.text("1000")
 UUID_GENERATE_V4 = sqlalchemy.text("uuid_generate_v4()")
@@ -43,6 +46,7 @@ FALSE = sqlalchemy.text("false")
 engine = sqlalchemy.create_engine(
     CONNECTION_STRING,
     connect_args=CONNECTION_ARGS,
+    poolclass=SingletonThreadPool,
     echo=True,  # Set to True to get SQL statements.
 )
 
