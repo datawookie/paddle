3ac1bbcbd63eb46c039fee43279520d2a5a0ebfa
Andrew Collier <andrew@fathomdata.dev>
2022-10-08 05:46:19 +0100

    feat: Separate team creation page

--- kanoe/views/team.py
+++ kanoe/views/team.py
@@ -1,16 +1,24 @@
 from .common import *
 
 
-@blueprint.route("/teams", methods=("GET", "POST"))
+@blueprint.route("/teams")
 def teams():
-    if request.method == "POST":
-        name = request.form["name"]
+    teams = session.query(db.Team).all()
+    return render_template("teams.j2", teams=teams)
 
-        team = db.Team(name=name)
+
+@blueprint.route("/team/create", methods=("GET", "POST"))
+def team_create():
+    if request.method == "POST":
+        print(request.form)
+        name = request.form["name"]
+        series_id = request.form["series"]
+
+        team = db.Team(name=name, series_id=series_id)
         session.add(team)
         session.commit()
 
         return redirect(url_for("kanoe.teams"))
 
-    teams = session.query(db.Team).all()
-    return render_template("teams.j2", teams=teams)
+    serieses = session.query(db.Series).order_by(db.Series.name.desc()).all()
+    return render_template("team-create.j2", serieses=serieses)
