55f42ff8c81c229aaecaf940a4df36d8b2ba0b54
Andrew Collier <andrew@fathomdata.dev>
2022-12-10 07:37:37 +0000

    feat: Add or edit paddler in separate form

--- kanoe/views/paddler.py
+++ kanoe/views/paddler.py
@@ -1,8 +1,27 @@
 from .common import *
 
 
-@blueprint.route("/paddlers", methods=("GET", "POST"))
+def empty_to_none(text):
+    if text == "":
+        return None
+    else:
+        return text
+
+
+@blueprint.route("/paddlers")
 def paddlers():
+    paddlers = session.query(db.Paddler).all()
+    return render_template("paddlers.j2", paddlers=paddlers)
+
+
+@blueprint.route("/paddler/<paddler_id>", methods=("GET", "POST"))
+@blueprint.route("/paddler/", defaults={"paddler_id": None}, methods=("GET", "POST"))
+def paddler(paddler_id):
+    if paddler_id:
+        paddler = session.query(db.Paddler).get(paddler_id)
+    else:
+        paddler = None
+
     if request.method == "POST":
         first = request.form["first"]
         middle = request.form["middle"]
@@ -27,31 +46,41 @@ def paddlers():
             flash("First name is required!", "danger")
         elif not last:
             flash("Last name is required!", "danger")
-        elif not division:
-            flash("Division is required!", "danger")
+        # elif not division:
+        #     flash("Division is required!", "danger")
         else:
-            paddler = db.Paddler(
-                first=first,
-                middle=middle,
-                last=last,
-                division=division,
-                dob=dob,
-                title=title,
-                emergency_name=emergency_name,
-                emergency_phone=emergency_phone,
-            )
-            session.add(paddler)
-            session.commit()
+            # Updating an existing paddler or creating a new one?
+            #
+            if paddler:
+                # Update existing paddler.
+                paddler.first = empty_to_none(first)
+                paddler.middle = empty_to_none(middle)
+                paddler.last = empty_to_none(last)
+                paddler.division = empty_to_none(division)
+                paddler.dob = empty_to_none(dob)
+                paddler.title = empty_to_none(title)
+                paddler.emergency_name = empty_to_none(emergency_name)
+                paddler.emergency_phone = empty_to_none(emergency_phone)
 
-            flash("Added a new paddler.", "success")
+                flash("Updated existing paddler.", "success")
+            else:
+                # Create new paddler.
+                paddler = db.Paddler(
+                    first=first,
+                    middle=middle,
+                    last=last,
+                    division=division,
+                    dob=dob,
+                    title=title,
+                    emergency_name=emergency_name,
+                    emergency_phone=emergency_phone,
+                )
+                session.add(paddler)
+
+                flash("Added a new paddler.", "success")
+
+            session.commit()
 
             return redirect(url_for("kanoe.paddlers"))
 
-    paddlers = session.query(db.Paddler).all()
-    return render_template("paddlers.j2", paddlers=paddlers)
-
-
-@blueprint.route("/paddler/<paddler_id>")
-def paddler(paddler_id):
-    paddler = session.query(db.Paddler).get(paddler_id)
     return render_template("paddler.j2", paddler=paddler)
