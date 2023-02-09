5c12b64d81605e8ddf659394a91d3e19290fa446
Andrew Collier <andrew@fathomdata.dev>
2023-02-09 07:01:38 +0000

    fix: Update & delete note on same page

--- kanoe/views/note.py
+++ kanoe/views/note.py
@@ -6,23 +6,18 @@ from .common import *
 
 @blueprint.route("/entry/note/add/<entry_id>", methods=("GET", "POST"))
 @login_required
-def entry_note_add(entry_id):
+def entry_note_update(entry_id):
+    entry = session.query(db.Entry).get(entry_id)
+
     if request.method == "POST":
-        note = request.form.get("note")
-        entry = session.query(db.Entry).get(entry_id)
-        entry.note = note
+        if "delete" in request.form:
+            entry.note = None
+        else:
+            note = request.form.get("note")
+            entry.note = note
+
         session.commit()
 
         return redirect(url_for("kanoe.entry", entry_id=entry.id))
 
-    return render_template("entry-note.j2")
-
-
-@blueprint.route("/entry/note/delete/<entry_id>", methods=("GET", "POST"))
-@login_required
-def entry_note_delete(entry_id):
-    entry = session.query(db.Entry).get(entry_id)
-    entry.note = None
-    session.commit()
-
-    return redirect(url_for("kanoe.entry", entry_id=entry.id))
+    return render_template("entry-note.j2", entry=entry)
