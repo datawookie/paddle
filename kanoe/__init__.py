091692c801481c8fc256bb0fec60f8a291f64e9c
Andrew Collier <andrew@fathomdata.dev>
2023-02-20 05:28:39 +0000

    feat: Add app factory function

--- kanoe/__init__.py
+++ kanoe/__init__.py
@@ -22,43 +22,47 @@ logging.basicConfig(
 logging.getLogger("fontTools").setLevel(logging.WARNING)
 logging.getLogger("weasyprint").setLevel(logging.ERROR)
 
-app = Flask(__name__, static_url_path="")
 
-# Secret key for sessions.
-#
-app.secret_key = b"8842f61bd2e13ad8285f57a551e2e6cb3521f38740ac0358e74077714eea0c82"
+def factory():
+    app = Flask(__name__, static_url_path="")
 
-# app.config["EXPLAIN_TEMPLATE_LOADING"] = True
-app.config["TEMPLATES_AUTO_RELOAD"] = True
-app.config["SECRET_KEY"] = os.urandom(24).hex()
+    # Secret key for sessions.
+    #
+    app.secret_key = b"8842f61bd2e13ad8285f57a551e2e6cb3521f38740ac0358e74077714eea0c82"
 
-# Initialise Flask-Bootstrap.
-#
-Bootstrap5(app)
+    # app.config["EXPLAIN_TEMPLATE_LOADING"] = True
+    app.config["TEMPLATES_AUTO_RELOAD"] = True
+    app.config["SECRET_KEY"] = os.urandom(24).hex()
 
-login_manager = LoginManager()
-#
-# login_manager.session_protection = "strong"
-# login_manager.login_view = "login"
-# login_manager.login_message_category = "info"
-#
-login_manager.init_app(app)
+    # Initialise Flask-Bootstrap.
+    #
+    Bootstrap5(app)
+
+    login_manager = LoginManager()
+    #
+    # login_manager.session_protection = "strong"
+    # login_manager.login_view = "login"
+    # login_manager.login_message_category = "info"
+    #
+    login_manager.init_app(app)
+
+    @login_manager.user_loader
+    def load_user(user_id):
+        # Reload user object from user ID stored in session.
+        return session.query(db.User).get(user_id)
+
+    # This will disable login required (use for development and testing).
+    #
+    app.config["LOGIN_DISABLED"] = True
+
+    Bcrypt(app)
+
+    app.register_blueprint(blueprint)
+
+    return app
 
 
-@login_manager.user_loader
-def load_user(user_id):
-    # Reload user object from user ID stored in session.
-    return session.query(db.User).get(user_id)
-
-
-# This will disable login required (use for development and testing).
-#
-app.config["LOGIN_DISABLED"] = True
-
-
-bcrypt = Bcrypt(app)
-
-app.register_blueprint(blueprint)
+app = factory()
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
