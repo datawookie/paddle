33c36ca409b55f87b2fad88a6659452ba382534f
Andrew Collier <andrew@fathomdata.dev>
2023-02-20 07:13:29 +0000

    feat: Add basic data to database

---
+++ tests/conftest.py
@@ -0,0 +1,63 @@
+import os
+import tempfile
+import logging
+import pytest
+
+import database as db
+from kanoe import factory
+
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s [%(levelname)7s] %(message)s",
+)
+
+# Temporary file for database.
+#
+_, DB_PATH = tempfile.mkstemp(".db")
+#
+os.environ["CONNECTION_STRING"] = f"sqlite:///{DB_PATH}"
+
+db.Base.metadata.create_all(db.engine)
+session = db.Session()
+
+session.add(db.Series(name="Series"))
+
+session.add(db.Club(id="GRY", name="Gryffindor"))
+session.add(db.Club(id="HUF", name="Hufflepuff"))
+
+session.add(db.TeamType(label="Junior"))
+session.add(db.TeamType(label="Senior"))
+
+for category in db.CATEGORY_LIST:
+    session.add(db.Category(label=category))
+
+for number in range(1, 21):
+    session.add(db.Number(id=number))
+
+
+session.commit()
+
+
+@pytest.fixture
+def app():
+    app = factory()
+    app.config.update(
+        {
+            "TESTING": True,
+        }
+    )
+
+    yield app
+
+    # Delete testing database.
+    # os.unlink(DB_PATH)
+
+
+@pytest.fixture
+def client(app):
+    return app.test_client()
+
+
+@pytest.fixture
+def runner(app):
+    return app.test_cli_runner()
