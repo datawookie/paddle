a8f3bf635c762f1216a5119fedda13ba4343dff1
Andrew Collier <andrew@fathomdata.dev>
2023-03-02 07:58:46 +0000

    feat: Wrangle series entries

--- kanoe/views/series.py
+++ kanoe/views/series.py
@@ -7,9 +7,47 @@ from .common import *
 @login_required
 def series(series_id):
     series = session.query(db.Series).get(series_id)
+    # Find past races in series.
+    races = session.query(db.Race.id).filter(db.Race.series_id == series_id)
+    # Find entries for races in series.
+    entries = session.query(db.Entry)
+    entries = (
+        entries.filter(db.Entry.race_id.in_(races))
+        .filter(db.Entry.time_start.is_not(None))
+        .filter(db.Entry.time_finish.is_not(None))
+    )
+    entries = entries.all()
+
+    races = session.query(db.Race).filter(db.Race.series_id == series_id).all()
+    races_past = len([race for race in races if race.past])
+
+    # Break down into categories.
+    #
+    categories = db.entries_get_categories(entries)
+
+    # Within each category group entries using paddler hash.
+    #
+    # The resulting data structure is:
+    #
+    # - dictionary of categories
+    #   - dictionary of crews
+    #       - list of entries
+    #
+    for category, entries in categories.items():
+        crews = {}
+        for entry in entries:
+            try:
+                crews[entry.crew_hash].append(entry)
+            except KeyError:
+                crews[entry.crew_hash] = [entry]
+        # Keep only crews who have finished all races in series.
+        crews = {k: v for k, v in crews.items() if len(v) == races_past}
+        categories[category] = crews
+
     return render_template(
         "series.j2",
         series=series,
+        categories=categories,
     )
 
 
