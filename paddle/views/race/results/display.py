287067c4b5dd4f1fbc601267035dddde4aaaf74a
Andrew Collier <andrew@fathomdata.dev>
2023-04-02 07:03:29 +0100

    fix: Remove old category results view

--- paddle/views/race/results/display.py
+++ paddle/views/race/results/display.py
@@ -2,50 +2,22 @@ from ...common import *
 from ...util import argument_boolean
 
 
-@blueprint.route("/race/<race_id>/results/category", methods=("GET", "POST"))
-def race_results_category(race_id):
-    race = session.get(db.Race, race_id)
-    categories = session.query(db.Category).all()
-    category = None
-    results = None
-
+@blueprint.route("/race/<race_id>/results", methods=("GET", "POST"))
+def race_results(race_id):
     if request.method == "POST":
-        category_id = int(request.form.get("category_id"))
-        category = session.get(db.Category, category_id)
-
-        results = (
-            session.query(db.Entry)
-            .filter(db.Entry.race_id == race_id)
-            .filter(db.Entry.time_start.is_not(None))
-            .filter(db.Entry.time_finish.is_not(None))
-            .filter(db.Entry.category_id == category_id)
-            .all()
+        # Process category selection from form.
+        category = int(request.form.get("category"))
+        return redirect(
+            url_for("kanoe.race_results", race_id=race_id, category=category)
         )
 
-        # Sort by finish time.
-        results.sort(key=lambda x: x.time, reverse=False)
-
-        # Keep only top 10.
-        results = results[:10]
-
-    return render_template(
-        "race-results-category.j2",
-        race=race,
-        category=category,
-        categories=categories,
-        results=results,
-    )
-
-
-@blueprint.route("/race/<race_id>/results/scrolling")
-def race_results_scrolling(race_id):
     scrolling = argument_boolean(request.args.get("scrolling", 0))
     try:
         # ?category -> Show category selector.
         # ?category=<category_id> -> Show category selector and selected category.
         #
         category = request.args["category"]
-        if category == "":
+        if category == "" or category == "0":
             category = True
         else:
             category = session.get(db.Category, category)
@@ -66,12 +38,13 @@ def race_results_scrolling(race_id):
     )
 
     data = db.entries_get_categories(results)
-    #
+    # Filter selected category.
     if isinstance(category, db.Category):
-        # Filter selected category.
         for key in list(data.keys()):
             if key != category.label:
                 del data[key]
+    elif category:
+        data = {}
 
     # Sort results in each category.
     for results in data.values():
@@ -80,7 +53,7 @@ def race_results_scrolling(race_id):
     announcements = session.query(db.Announcement).filter(db.Announcement.enabled).all()
 
     return render_template(
-        "race-results-scrolling.j2",
+        "race-results.j2",
         scrolling=scrolling,
         category=category,
         categories=categories,
