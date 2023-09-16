fa05b4294ecfcff4bfd94dfa68fa9c7eafe4f506
Andrew Collier <andrew@fathomdata.dev>
2023-09-16 07:34:05 +0100

    feat: Improve formatting

--- paddle/views/race/results/export.py
+++ paddle/views/race/results/export.py
@@ -6,7 +6,7 @@ import tempfile
 
 from flask_weasyprint import render_pdf
 from openpyxl import Workbook
-from openpyxl.styles import Font
+from openpyxl.styles import Alignment, Font, PatternFill
 
 from ...common import *
 
@@ -74,14 +74,8 @@ def race_results_export_xlsx(race_id):
         results.sort(key=lambda x: x.time, reverse=False)
 
     workbook = Workbook()
-    font_data = Font(
-        name="Arial",
-        size=10,
-    )
-    font_head = Font(
-        name="Courier New",
-        size=13,
-    )
+    font_head = Font(name="Arial", size=10, color="FFFFFF")
+    font_data = Font(name="Arial", size=10, color="000000")
 
     # Delete default sheet.
     workbook.remove_sheet(workbook.get_sheet_by_name("Sheet"))
@@ -93,21 +87,21 @@ def race_results_export_xlsx(race_id):
         sheet = workbook.create_sheet(category)
 
         # Set column widths.
-        sheet.column_dimensions["A"].width = 10  # Number
+        sheet.column_dimensions["A"].width = 6  # Number
         sheet.column_dimensions["B"].width = 20  # Surname
         sheet.column_dimensions["C"].width = 20  # First name
-        sheet.column_dimensions["D"].width = 10  # BCU number
+        sheet.column_dimensions["D"].width = 9  # BCU number
         sheet.column_dimensions["E"].width = 10  # Expiry
-        sheet.column_dimensions["F"].width = 25  # Club
-        sheet.column_dimensions["G"].width = 10  # Class
-        sheet.column_dimensions["H"].width = 10  # Div
-        sheet.column_dimensions["I"].width = 10  # Due
-        sheet.column_dimensions["J"].width = 10  # Paid
-        sheet.column_dimensions["K"].width = 15  # Start
-        sheet.column_dimensions["L"].width = 15  # Finish
-        sheet.column_dimensions["M"].width = 15  # Elapsed
-        sheet.column_dimensions["N"].width = 10  # Position
-        sheet.column_dimensions["O"].width = 10  # Points
+        sheet.column_dimensions["F"].width = 5  # Club
+        sheet.column_dimensions["G"].width = 5  # Class
+        sheet.column_dimensions["H"].width = 5  # Div
+        sheet.column_dimensions["I"].width = 5  # Due
+        sheet.column_dimensions["J"].width = 5  # Paid
+        sheet.column_dimensions["K"].width = 7  # Start
+        sheet.column_dimensions["L"].width = 7  # Finish
+        sheet.column_dimensions["M"].width = 7  # Elapsed
+        sheet.column_dimensions["N"].width = 7  # Position
+        sheet.column_dimensions["O"].width = 5  # Points
         sheet.column_dimensions["P"].width = 5  # P/D
 
         # Add header records.
@@ -133,9 +127,7 @@ def race_results_export_xlsx(race_id):
 
         # Add results.
         for position, result in enumerate(results):
-            print(dir(result))
             for crew in result.crews:
-                print(crew.paddler.age_group)
                 row = [
                     int(result.race_number),
                     crew.paddler.last,
@@ -158,11 +150,22 @@ def race_results_export_xlsx(race_id):
                 ]
                 sheet.append(row)
 
-        for cells in sheet.iter_rows(
-            min_row=0, max_row=sheet.max_row, min_col=1, max_col=sheet.max_column
-        ):
+        for cells in sheet.iter_rows(max_row=sheet.max_row, max_col=sheet.max_column):
             for cell in cells:
-                cell.font = font_head if cell.row == 1 else font_data
+                if cell.row == 1:
+                    cell.font = font_head
+                    # Style for header row.
+                    cell.alignment = Alignment(horizontal="center")
+                    cell.fill = PatternFill(
+                        start_color="333333", end_color="333333", fill_type="solid"
+                    )
+                else:
+                    cell.font = font_data
+                    # Styles for rest of rows.
+                    if cell.column_letter in ("E", "K", "L", "M"):
+                        cell.alignment = Alignment(horizontal="right")
+                    if cell.column_letter in ("F", "H", "N"):
+                        cell.alignment = Alignment(horizontal="center")
 
     workbook.save(path)
 
