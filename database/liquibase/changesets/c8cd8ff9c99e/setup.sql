1ce6e9c0fe074cdc4f0cdcbff38d14009643bc08
Andrew Collier <andrew@fathomdata.dev>
2023-09-23 06:38:14 +0100

    feat: Add membership bodies

---
+++ database/liquibase/changesets/c8cd8ff9c99e/setup.sql
@@ -0,0 +1,6 @@
+-- Running upgrade e3e94e9034a1 -> 56711208cd56
+
+insert into membership_body (id, acronym, name) values (1, 'BC', 'British Canoeing');
+insert into membership_body (id, acronym, name) values (2, 'SCA', 'Scottish Canoe Association');
+insert into membership_body (id, acronym, name) values (3, 'CW', 'Canoe Wales');
+insert into membership_body (id, acronym, name) values (4, 'CANI', 'Canoe Association of Northern Ireland');
