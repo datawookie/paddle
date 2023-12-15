126d963323111ade93151e39cdf183015a4f50ff
Andrew Collier <andrew@fathomdata.dev>
2023-12-15 15:02:56 +0000

    fix: Remove page for updating note

--- paddle/views/__init__.py
+++ paddle/views/__init__.py
@@ -1,14 +1,12 @@
-from .common import *
-
+from .announcement import *
 from .api import *
 from .club import *
+from .common import *
+from .crew import *
 from .entry import *
 from .member import *
 from .paddler import *
 from .race import *
-from .crew import *
 from .series import *
 from .team import *
 from .user import *
-from .note import *
-from .announcement import *
