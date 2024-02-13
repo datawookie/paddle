263c8e51f4ac774120296aeb2897bc6d17ecb856
Andrew Collier <andrew@fathomdata.dev>
2024-02-13 16:21:31 +0000

    fix: Sorting out migrations

--- database/liquibase/changesets/c8cd8ff9c99e/setup.sql
+++ database/liquibase/changesets/c8cd8ff9c99e/setup.sql
@@ -1,6 +1,8 @@
--- Running upgrade e3e94e9034a1 -> 56711208cd56
+-- Running upgrade 56711208cd56 -> c8cd8ff9c99e
 
 insert into membership_body (id, acronym, name) values (1, 'BC', 'British Canoeing');
 insert into membership_body (id, acronym, name) values (2, 'SCA', 'Scottish Canoe Association');
 insert into membership_body (id, acronym, name) values (3, 'CW', 'Canoe Wales');
 insert into membership_body (id, acronym, name) values (4, 'CANI', 'Canoe Association of Northern Ireland');
+
+UPDATE alembic_version SET version_num='c8cd8ff9c99e' WHERE alembic_version.version_num = '56711208cd56';
