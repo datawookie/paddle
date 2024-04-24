d547e4ad8cfc74668c34534d2d88153afd039de1
Andrew Collier <andrew@fathomdata.dev>
2024-04-24 13:20:26 +0100

    fix: Clean up unused files

--- load-race-data.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-./2-load-entries.py --race-name="Race A" --race-date="2022-02-01" race-A-entries.json
-./2-load-entries.py --race-name="Race B" --race-date="2022-03-01" race-B-entries.json
-./2-load-entries.py --race-name="Race C" --race-date="2022-04-01" race-C-entries.json
-./2-load-entries.py --race-name="Race D" --race-date="2022-05-01" race-D-entries.json
